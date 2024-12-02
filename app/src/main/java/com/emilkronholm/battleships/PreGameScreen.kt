4d33f1a10e58786216b635397a2fcfcb1279c1b7
ekkecode <emil.kronholm@gmail.com>
2024-12-02 18:19:38 +0100

    Create preGameScreen

---
+++ app/src/main/java/com/emilkronholm/battleships/PreGameScreen.kt
@@ -0,0 +1,9 @@
+package com.emilkronholm.battleships
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun PreGameScreen(navController: NavController, playerViewModel: PlayerViewModel) {
+    Grid()
+}
