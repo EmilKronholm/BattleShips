8e02972e76e5821d13416f5cda3a65b39d1f992b
ekkecode <emil.kronholm@gmail.com>
2024-12-11 21:10:40 +0100

    Implement move, drag, and tapping
    NOT WORKING ATM

--- app/src/main/java/com/emilkronholm/battleships/BoardComposables.kt
+++ app/src/main/java/com/emilkronholm/battleships/BoardComposables.kt
@@ -1,8 +1,9 @@
     package com.emilkronholm.battleships
-
     import android.widget.Toast
     import androidx.compose.foundation.background
     import androidx.compose.foundation.clickable
+    import androidx.compose.foundation.gestures.detectDragGestures
+    import androidx.compose.foundation.gestures.detectTapGestures
     import androidx.compose.foundation.layout.Box
     import androidx.compose.foundation.layout.aspectRatio
     import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,7 +17,14 @@
     import androidx.compose.runtime.remember
     import androidx.compose.runtime.setValue
     import androidx.compose.ui.Modifier
+    import androidx.compose.ui.geometry.Rect
     import androidx.compose.ui.graphics.Color
+    import androidx.compose.ui.input.pointer.changedToDown
+    import androidx.compose.ui.input.pointer.changedToUp
+    import androidx.compose.ui.input.pointer.pointerInput
+    import androidx.compose.ui.layout.boundsInRoot
+    import androidx.compose.ui.layout.onGloballyPositioned
+    import androidx.compose.ui.platform.LocalConfiguration
     import androidx.compose.ui.platform.LocalContext
     import androidx.compose.ui.unit.dp
 
@@ -24,43 +32,115 @@
     fun Grid(board: Board) {
 
         var selectedBoat : Ship? by remember { mutableStateOf(null) }
-        var offset by remember { mutableIntStateOf(0) }
-        val context = LocalContext.current
+        var offset by remember { mutableStateOf(Coordinate(0, 0)) }
         val invalidCoordinates = board.getListOfInvalidCoordinates()
         println(board.ships)
         println(invalidCoordinates)
 
+        val configuration = LocalConfiguration.current
+        var rect : Rect = Rect(0f, 0f, 0f, 0f)
+
         LazyVerticalGrid(
             columns = GridCells.Fixed(10),
-            modifier = Modifier.padding(8.dp).background(Color(70, 21, 100, 200))
-        ) {
+            modifier = Modifier
+                .padding(8.dp)
+                .background(Color(70, 21, 100, 200))
+                .onGloballyPositioned { layoutCoordinates ->
+                    rect = layoutCoordinates.boundsInRoot()
+                }
+                .pointerInput(Unit) {
+                    awaitPointerEventScope {
+
+                        var hasMoved = false;
+
+                        while (true) {
+                            val event = awaitPointerEvent()
+                            val currentPosition = event.changes.first().position
+
+                            when {
+                                event.changes.any { it.changedToDown() } -> {
+
+                                }
+                                event.changes.any { it.changedToUp() } -> {
+                                    //Rotate boat if...
+                                    if (!hasMoved) {
+                                        val x = (currentPosition.x / (rect.size.width / 10)).toInt()
+                                        val y = (currentPosition.y / (rect.size.height / 10)).toInt()
+                                        val coordinate = Coordinate(x, y)
+
+                                        val ship = board.getShipAt(coordinate)
+                                        if (ship != null)
+                                        {
+                                            board.ships.remove(ship.ship)
+                                            ship.ship.rotate()
+                                            board.ships.add(0, ship.ship)
+                                        }
+                                    }
+
+                                    hasMoved = false;
+                                    selectedBoat = null
+                                }
+                                event.changes.any { it.pressed } -> {
+                                    // Dragging gesture detected
+                                    val x = (currentPosition.x / (rect.size.width / 10)).toInt()
+                                    val y = (currentPosition.y / (rect.size.height / 10)).toInt()
+                                    val coordinate = Coordinate(x, y)
+
+                                    println("Dragging at grid coordinates: $coordinate")
+
+                                    if (selectedBoat != null && selectedBoat!!.parts[0].coordinate != coordinate + offset) {
+                                        hasMoved = true
+                                        board.ships.remove(selectedBoat!!)
+                                        selectedBoat?.moveShipTo(coordinate + offset)
+                                        board.ships.add(0, selectedBoat!!)
+                                        println("Boat moved to $coordinate")
+                                    } else {
+                                        val selectedShipInfo = board.getShipAt(coordinate)
+                                        selectedBoat = selectedShipInfo?.ship
+                                        if (selectedShipInfo != null) {
+                                            offset = if (selectedBoat!!.isVertical) {
+                                                Coordinate(0, selectedShipInfo.headPos.y - y)
+                                            } else {
+                                                Coordinate(selectedShipInfo.headPos.x - x, 0)
+                                            }
+                                            println("Boat selected: $selectedBoat")
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                })
+            {
             items(100) { index ->
-                val coordinate = Coordinate(index/10, index%10)
+                val coordinate = Coordinate(index%10, index/10)
                 val state = if (invalidCoordinates.contains(coordinate))
                     BoardSquareState.HIT else board.getState(coordinate)
 
                 GridItem(index, state) {
-                    //If null, try to select a new boat
-                    if (selectedBoat == null)
-                    {
-                        selectedBoat = board.getShipAt(coordinate)
-                        println("Tried to get boat at $coordinate")
-                    }
-                    //If not null, try to move selected boat
-                    else
-                    {
-                        val pos = selectedBoat!!.parts[0].coordinate
 
-                        if (pos == coordinate)
-                        {
-                            selectedBoat!!.isVertical = !selectedBoat!!.isVertical
-                        }
 
-                        board.ships.remove(selectedBoat!!)
-                        selectedBoat?.moveShipTo(coordinate)
-                        board.ships.add(0, selectedBoat!!)
-                        selectedBoat = null
-                    }
+//                    //If null, try to select a new boat
+//                    if (selectedBoat == null)
+//                    {
+//                        selectedBoat = board.getShipAt(coordinate)
+//                        println("Tried to get boat at $coordinate")
+//                    }
+//                    //If not null, try to move selected boat
+//                    else
+//                    {
+//                        val pos = selectedBoat!!.parts[0].coordinate
+//
+//                        if (pos == coordinate)
+//                        {
+//                            selectedBoat!!.isVertical = !selectedBoat!!.isVertical
+//                        }
+//
+//                        board.ships.remove(selectedBoat!!)
+//                        selectedBoat?.moveShipTo(coordinate)
+//                        board.ships.add(0, selectedBoat!!)
+//                        selectedBoat = null
+//                    }
                 }
             }
         }
@@ -85,8 +165,9 @@
                 .aspectRatio(1f)
                 .padding(2.dp)
                 .background(color)
-                .clickable {
-                    onClick()
-                }
+
+//                .clickable {
+//                    onClick()
+//                }
         )
     }
